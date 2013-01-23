cbabd7a39cc76021743fe47e1fa783473062c080
suraj <sbhagavan@brandintegrity.com>
2013-01-23 16:21:56 -0500

    testing merge functionality

--- git2/Program.cs
+++ git2/Program.cs
@@ -11,6 +11,7 @@ namespace git2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("I love hotfixes");
         }
     }
 }
